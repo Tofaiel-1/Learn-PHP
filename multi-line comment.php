9af87019f1d08bc6b24feec703f71bb0b1a95f31
Md tofaiel <mdtofaielhussaintota@gmail.com>
2025-06-11 22:48:01 +0600

    Here code is multi-line comment

---
+++ multi-line comment.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+  <?php
+  /*
+echo "Welcome to my home!";
+echo "Mi casa su casa!";
+*/
+  echo "Multi-line comment above. This is simple code for using multi-line comment in php";
+  ?>
+
+</body>
+
+</html>
